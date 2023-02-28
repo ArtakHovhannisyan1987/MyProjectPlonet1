c5ce2c78e31f5cb063b0dc494631069208195d47
ArtakHovhannisyan1987 <artakhovhannisyan374>
2023-02-28 14:29:55 +0400

    ubd. app.js + ubd. Footer + ubd. HomePage + ubd. public

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import "./HomePage.css";
 
 
 export default function Home(){
+    const [count, setCount] = useState(false);
+    const [nkar, setNkar] = useState(false);
+    const [real, setReal] = useState(false);
     return (
         <div>
-            <h1>Home</h1>
+            <div className='homeflex'>
+                <div className='div'>
+                    <div className='home-bg-1' onClick={()=>setCount((e)=>!e)}></div>
+                    { count && <NavLink to={"https://en.wikipedia.org/wiki/Armenia"} className='home-h2'>Anciend Armenia</NavLink>}
+                </div>
+                <div className='div'>
+                    <div className='home-bg-2' onClick={()=>setNkar((e)=>!e)}></div>
+                    { nkar && <NavLink to={"https://ru.wikipedia.org/wiki/%D0%A2%D1%83%D1%80%D0%B8%D0%B7%D0%BC_%D0%B2_%D0%90%D1%80%D0%BC%D0%B5%D0%BD%D0%B8%D0%B8"} className='home-h2'>Tourism in Armenia</NavLink>}
+                </div>
+                <div className='div'>
+                    <div className='home-bg-3' onClick={()=>setReal((e)=>!e)}></div>
+                    {real && <NavLink to={"https://en.wikipedia.org/wiki/Car"} className='home-h2'>Car</NavLink>}
+                </div>
+            </div>
         </div>
     )
 }
